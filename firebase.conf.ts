886012c158b73918d5fe8ab90f0fc76ee7e9e307
Fordjour12 <fordjourbobiem@gmail.com>
2023-08-29 14:09:21 +0000

    fix(firebase-conf): fixing of api

--- firebase.conf.ts
+++ firebase.conf.ts
@@ -7,27 +7,16 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.EXPO_PUBLIC_APIKEY,
-//   authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
-//   projectId: process.env.EXPO_PUBLIC_PROJECTID,
-//   storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
-//   messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
-//   appId: process.env.EXPO_PUBLIC_APPID,
-//   measurementId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
-// };
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBS65bI1zZCA8bSBLxwJZGtUSIFSTGF0_E",
-  authDomain: "real-estate-7ea.firebaseapp.com",
-  projectId: "real-estate-7ea",
-  storageBucket: "real-estate-7ea.appspot.com",
-  messagingSenderId: "104725137700",
-  appId: "1:104725137700:web:ab266e36ba3aa1a19d0bb1",
-  measurementId: "G-K2DWBRQEV9",
+  apiKey: process.env.EXPO_PUBLIC_APIKEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECTID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.EXPO_PUBLIC_APPID,
+  measurementId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
